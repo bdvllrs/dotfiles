f93f72610707434ff78b5aab045aa100b00b71f3
bdvllrs <bdvllrs@gmail.com>
2020-05-26 14:47:09 +0200

    updated confs

--- .config/i3/monitor-connect.sh
+++ .config/i3/monitor-connect.sh
@@ -1,5 +1,7 @@
-EXT=HDMI-2
-INT=eDP-1
+EXT=HDMI2
+INT=eDP1
 
-xrandr | grep 'HDMI-2 connected' && xrandr --output "${INT}" --auto --pos 0x2160 --scale 0.9999x0.9999 --primary --output "${EXT}" --auto --scale 1.9x1.9 --pos 0x0
-xrandr | grep 'HDMI-2 disconnected' && xrandr --output "${EXT}" --off
+xrandr | grep 'HDMI2 connected' && xrandr --output "${INT}" --auto --pos 0x2160 --scale 0.9999x0.9999 --primary --output "${EXT}" --auto --scale 1.9x1.9 --pos 0x0
+xrandr | grep 'HDMI2 disconnected' && xrandr --output "${EXT}" --off
+
+killall polybar; polybar -r default & polybar -r right
--- .config/i3/update_wallpaper.sh
+++ .config/i3/update_wallpaper.sh
@@ -1,5 +1,14 @@
-feh --randomize --bg-fill ~/Pictures/wallpapers/*
-while sleep 3600
-do
-	feh --randomize --bg-fill ~/Pictures/wallpapers/*
-done
+# feh --randomize --bg-fill ~/Pictures/wallpapers/*
+feh --bg-fill ~/Pictures/wallpapers/paint_stains_purple_118346_3840x2400.jpg
+
+
+# if [ $# -gt 1 ]
+# then
+#     if [ "$1" == "live" ]
+#     then
+#         while sleep 3600
+#         do
+#             feh --randomize --bg-fill ~/Pictures/wallpapers/*
+#         done
+#     fi
+# fi
